f417f8ead2479bdd48407079e1099de749fa15a7
Rebecca Meritz <rebecca@meritz.com>
2021-09-21 13:17:43 -0700

    Add ablity to update and delete annotations

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,5 +1,36 @@
 class AnnotationsController < ApplicationController
+  before_action :set_annotation, only: [:edit, :destroy, :update]
+
   def index
     @annotations = Annotation.scan
+  end
+
+  def edit
+  end
+
+  def destroy
+    @annotation.delete!
+    redirect_to annotations_path, alert: "Deleted annotation #{@annotation.coordinate}."
+  end
+
+  def update
+    @annotation.assign_attributes(annotation_params)
+    @annotation.save
+    redirect_to annotations_path, alert: "Updated annotation #{@annotation.coordinate}."
+  end
+
+  private
+
+  def set_annotation
+    @annotation = Annotation.query(
+      key_condition_expression: "coordinate = :coordinate",
+      expression_attribute_values: {':coordinate': params[:coordinate]},
+      limit: 1
+    ).first
+    raise Aws::Record::Errors::NotFound unless @annotation.present?
+  end
+
+  def annotation_params
+    params.require(:annotation).permit(:Annotation)
   end
 end
--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,6 +1,11 @@
 class Annotation
+  include ActiveModel::Model
   include Aws::Record
   set_table_name "TrialTable"
-  string_attr :coordinate
+  string_attr :coordinate, hash_key: true
   string_attr :Annotation
+
+  def to_param
+    coordinate
+  end
 end
