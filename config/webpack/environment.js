3f8629f899e20be5242df4457caa7ff008b593e1
Rebecca Meritz <rebecca@meritz.com>
2021-09-21 19:53:10 -0700

    Remove PostCSS from Webpacker
    The postcss-loader does not understand how to load SCSS. It jumps the
    load order and ends up being given files that it cannot process.
    
    We don't even want it, so disable it.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
+for (loader of environment.loaders) {
+  loader.value.use = loader.value.use.filter(
+    function(e) { return e.loader != "postcss-loader" }
+  )
+}
+
 module.exports = environment
