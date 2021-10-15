2d3caa3bcebd8df5c46540ee1b3a5b3bf38e931e
Rebecca Meritz <rebecca@meritz.com>
2021-10-15 05:48:41 -0700

    Refactor user "authentication"
    Authentication is currently just loading the User object from the
    database and check that one exists.

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,42 +1,7 @@
 class AnnotationsController < ApplicationController
-  #before_action :set_annotation, only: [:edit, :destroy, :update]
-  before_action :set_user
+  before_action :authenticate_user!
 
   def index
-    redirect_to new_session_path, alert: "User could not be found" unless @user.valid?
     @annotated_objects = @user.annotated_objects
-  end
-
-  #def edit
-  #end
-
-  #def destroy
-  #  @annotation.delete!
-  #  redirect_to annotations_path, alert: "Deleted annotation #{@annotation.coordinate}."
-  #end
-
-  #def update
-  #  @annotation.assign_attributes(annotation_params)
-  #  @annotation.save
-  #  redirect_to annotations_path, alert: "Updated annotation #{@annotation.coordinate}."
-  #end
-
-  private
-
-  #def set_annotation
-  #  @annotation = Annotation.query(
-  #    key_condition_expression: "coordinate = :coordinate",
-  #    expression_attribute_values: {':coordinate': params[:coordinate]},
-  #    limit: 1
-  #  ).first
-  #  raise Aws::Record::Errors::NotFound unless @annotation.present?
-  #end
-
-  #def annotation_params
-  #  params.require(:annotation).permit(:Annotation)
-  #end
-  #
-  def set_user
-    @user = User.new(username: params[:user_username])
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def authenticate_user!
+    @user = User.new(username: params[:user_username])
+    redirect_to new_session_path, alert: "User could not be found" unless @user.valid?
+  end
+
   def record_not_found
     render plain: "404 Not Found", status: 404
   end
